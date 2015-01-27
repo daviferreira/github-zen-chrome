3e535032eae622c3ede72a444985a6fcd3e53d83
Davi Ferreira <contato@daviferreira.com>
2015-01-27 21:42:11 +0100

    Formatting

--- zen.js
+++ zen.js
@@ -1,8 +1,8 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.github.com/zen", true);
+xhr.open('GET', 'https://api.github.com/zen', true);
 xhr.onreadystatechange = function() {
-  if ( xhr.readyState == 4 ) {
+  if (xhr.readyState === 4) {
         document.getElementById('quote').innerText = xhr.responseText;
     }
-}
+};
 xhr.send();
